75989ef87097c97fb7c0272d38f561015ef74cc0
Anjolaoluwa <adebayoanjola@gmail.com>
2021-03-17 22:02:30 +0100

    fixed debugging display issue on mobile

--- components/queries.vue
+++ components/queries.vue
@@ -6,7 +6,7 @@
     <header v-if="!loading" class="w-full flex items-start justify-between">
       <div class="flex w-3/5 md:w-1/2 justify-around border-b">
         <h3 class="mr-1 md:mr-0">
-          <button class="text-sm md:text-xl px-2" :class="{'border-b-2 border-blue-500':currentTabComponent=='result'}" type="button" @click="currentTabComponent = 'result'">
+          <button class="text-sm md:text-xl px-2" :class="{'border-b-2 border-blue-500':currentTabComponent=='result'}" type="button" @click="currentTabComponent='result'">
             Query result
           </button>
         </h3>
@@ -27,6 +27,7 @@
     </header>
     <article v-if="!loading">
       <component :is="currentTabComponent" />
+      <p> this is a test component </p>
     </article>
   </section>
 </template>
@@ -40,9 +41,6 @@ export default {
   },
   computed: mapGetters(['loading']),
   methods: {
-    saveQuery () {
-
-    }
   }
 }
 </script>
